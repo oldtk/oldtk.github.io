0736cce7e7568e7bbc5348a621a58e3bcc9c82a2
Thanasis Kyprianos <than20022004@gmail.com>
2024-02-14 17:24:46 +0200

    What the hell am I doing

--- oldtk/oldtk.github.io/soundboard/scripts/keyboard.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('keydown', function (event) {
-  if (enggreek.includes(event.key)) {
-    const path = 'sounds/' + keys[event.key][isLowerCase(event.key) ? 0 : 1] + '.aac'
-    playSound(path);
-  }
-});
